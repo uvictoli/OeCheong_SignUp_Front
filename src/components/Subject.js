6e222b11b684011508751774b57346a31da529f0
Yoo SeungLi <69826406+uvictoli@users.noreply.github.com>
2021-05-18 19:38:11 +0900

    css update1

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import './Subject.css'
 
 class Subject extends Component {
     render() {
     return (
-        <h1><a href="/" onClick={function(e){
+        <h1><a href="/" 
+            class="Subject"
+            onClick={function(e){
             e.preventDefault();
             this.props.onChangePage();
-        }.bind(this)}>회원가입</a></h1>
+        }.bind(this)}
+        >
+        회원가입
+        </a></h1>
     )
     }
 }
